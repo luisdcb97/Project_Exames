8c771b2cd5eeea31cb54781560ee98244e1a0a32
Luis <luisdcb97@gmail.com>
2016-11-07 20:18:57 +0000

    Add files via upload

--- Sala.java
+++ Sala.java
@@ -1,3 +1,4 @@
+package project;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -26,8 +27,7 @@ public class Sala {
         id = name;
         //turn into method↓
         Date dat = new Date(y, m, d, h, min);
-        timeslots.add(dat);
-        duration.add(dur);
+        reserveSala(dat,dur);
     }
     
     public Sala(String name, ArrayList<Date> dt, ArrayList<Integer> dur){
@@ -76,6 +76,10 @@ public class Sala {
         }
         
         return false;
+    }
+    
+    public String getId(){
+        return this.id;
     }
     
 }
